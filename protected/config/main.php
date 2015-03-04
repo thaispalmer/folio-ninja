e9165575120a82149f066b5f443c8d55251ce9ed
Athos Palmer <athos@inbox.com>
2015-03-03 21:46:12 -0300

    Email de suporte@folio.ninja configurado para o formulário de contato

--- protected/config/main.php
+++ protected/config/main.php
@@ -126,7 +126,7 @@ return array(
 	// using Yii::app()->params['paramName']
 	'params'=>array(
 		// this is used in contact page
-		'contactEmail'=>'athos@interativadigital.com.br',
+		'contactEmail'=>'support@folio.ninja',
         'uploadFolder'=>'/upload/'
 	),
 );
